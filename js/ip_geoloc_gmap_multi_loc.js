d4f54a968de3606c0734d8086759bb72848eb1f7
Rik de Boer <rikdeboer1@yahoo.com.au>
2011-08-31 13:24:36 +1000

    Added "Visitor #" to marker mouse-over. @todo: marker colour for most recent visitor (you).

--- js/ip_geoloc_gmap_multi_loc.js
+++ js/ip_geoloc_gmap_multi_loc.js
@@ -7,7 +7,7 @@
       var mapOptions = {
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         disableDefaultUI: true,
-        zoom: 1,
+        zoom: 2,
         zoomControl: true
       };
       map = new google.maps.Map(document.getElementById(settings.ip_geoloc_div_map), mapOptions);
@@ -19,10 +19,12 @@
         var position = new google.maps.LatLng(locations[ip].latitude, locations[ip].longitude);
         if (i++ == 0) { // use to first, i.e. most recent, visitor to center the map
           map.setCenter(position);
+          marker = new google.maps.Marker({ map: map, position: position, title: Drupal.t('Visitor #') + i + ': ' + Drupal.t('you?') });
+        } else {
+          marker = new google.maps.Marker({ map: map, position: position, title: Drupal.t('Visitor #') + i });
         }
-        var marker = new google.maps.Marker({ map: map, position: position, title: Drupal.t('Location #') + i });
         balloonTexts['LL' + position] = Drupal.t('IP address: ') + ip + '<br/>' + locations[ip].formatted_address + '<br/>'
-          + Drupal.t('# visits: ') + locations[ip].visit_count + ', ' + Drupal.t('last visit: ') + locations[ip].last_visit;
+          + Drupal.t('#visits: ') + locations[ip].visit_count + ', ' + Drupal.t('last visit: ') + locations[ip].last_visit;
         google.maps.event.addListener(marker, 'click',  function(event) {
           new google.maps.InfoWindow({ content: "Lat/long: " + event.latLng + "<br/>" + balloonTexts['LL' + event.latLng], position: event.latLng }).open(map);
         });
