139a03d989a2c93cb38828fec225678634769c0f
RdeBoer <rikdeboer1@yahoo.com.au>
2014-11-01 14:18:44 +1100

    Added zoomlevel indicator and scale. Made sure that any of these only appear on the maps requested, rather than on all maps on the page

---
+++ js/ip_geoloc_leaflet_controls.js
@@ -0,0 +1,77 @@
+
+// Inspired by Roger Codina and
+// http://gis.stackexchange.com/questions/87199/how-does-one-reset-map-fitbounds-zoomtofeature-to-the-original-zoom
+
+(function ($) {
+
+  Drupal.behaviors.leafletAddMapReset = {
+    attach: function (context, settings) {
+      var maps = settings.leaflet;
+      for (var i = 0; i < maps.length; i++) {
+        var settings = maps[i].map.settings;
+        if (settings.zoomIndicator) {
+          maps[i].lMap.addControl(new L.control.zoomIndicator(settings.zoomIndicator));
+        }
+        if (settings.resetControlCss) {
+          maps[i].lMap.addControl(L.control.reset(settings.resetControlCss));
+        }
+        if (settings.scaleControl) {
+          maps[i].lMap.addControl(L.control.scale(settings.scaleControl));
+        }
+      }
+    }
+  }
+
+})(jQuery);
+
+L.Control.Reset = L.Control.extend({
+
+  options: {
+    position: 'topleft',
+    label: 'R'
+  },
+
+  onAdd: function(map) {
+    map._initialBounds = map.getBounds();
+
+    var button = L.DomUtil.create('a', 'leafet-control-reset leaflet-bar');
+    if (this.options.label.length <= 2) {
+      button.innerHTML = this.options.label;
+    }
+    else {
+      L.DomUtil.addClass(button, this.options.label);
+    }
+    button.setAttribute('title', Drupal.t('Reset'));
+    button.onclick = function() {
+      map.fitBounds(map._initialBounds);
+    };
+    return button;
+  },
+});
+
+L.control.reset = function(options) {
+  return new L.Control.Reset(options);
+};
+
+L.Control.ZoomIndicator = L.Control.extend({
+
+  options: {
+    position: 'topleft'
+  },
+
+  onAdd: function(map) {
+    this.indicator = L.DomUtil.create('div', 'leafet-control-zoom-indicator leaflet-bar');
+    this.indicator.setAttribute('title', Drupal.t('Zoom level'));
+    this.indicator.innerHTML = 'z' + map.getZoom();
+    map.on('zoomend', this.update, this);
+    return this.indicator;
+  },
+
+  update: function(event) {
+    this.indicator.innerHTML = event.target.getZoom();
+  }
+});
+
+L.control.zoomIndicator = function(options) {
+  return new L.Control.ZoomIndicator(options);
+};
