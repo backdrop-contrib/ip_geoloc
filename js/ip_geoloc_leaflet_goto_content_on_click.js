b407a945794768c4644d3f0dfb685cf09296b21c
RdeBoer <rikdeboer1@yahoo.com.au>
2015-12-05 09:16:15 +1100

    Issue #2629164 by RdeBoer: Pop up balloon over marker on mouse hover, rather than mosue click

--- js/ip_geoloc_leaflet_goto_content_on_click.js
+++ js/ip_geoloc_leaflet_goto_content_on_click.js
@@ -1,5 +1,6 @@
 
 jQuery(document).bind('leaflet.feature', function(event, marker, feature) {
+  
   // feature.feature_id is the node ID, as set by ip_geoloc_plugin_style_leaflet.inc
   // see also ip_geoloc_leaflet_sync_content.js
   if (feature.feature_id) {
@@ -7,6 +8,24 @@ jQuery(document).bind('leaflet.feature', function(event, marker, feature) {
   }
 
   marker.on('click', function(e) {
-    document.location.href = Drupal.settings.basePath + 'node/' + this.feature_id;
+    var leafletSettings = Drupal.settings.leaflet;
+    for (var i = 0; i < leafletSettings.length; i++) {
+      if ((this._map === leafletSettings[i].lMap) && leafletSettings[i].map.settings.gotoContentOnClick) {
+        document.location.href = Drupal.settings.basePath + 'node/' + this.feature_id;
+        return;
+      }
+    }
   });
+
+  marker.on('mouseover', function(e) {
+    var leafletSettings = Drupal.settings.leaflet;
+    for (var i = 0; i < leafletSettings.length; i++) {
+      if ((this._map === leafletSettings[i].lMap) && leafletSettings[i].map.settings.openBalloonsOnHover) {
+        marker.openPopup();
+        return;
+      }
+    }
+  });
+
 });
+
