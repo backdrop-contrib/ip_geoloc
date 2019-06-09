37f96bbeb6d5bf2a177e5ac4241e5241e5bdc906
RdeBoer <rikdeboer1@yahoo.com.au>
2019-06-09 14:04:48 +1000

    Add option to use TweenMax for Polygon Shadow Animation

--- js/ip_geoloc_leaflet_hover.js
+++ js/ip_geoloc_leaflet_hover.js
@@ -11,45 +11,56 @@ jQuery(document).bind('leaflet.feature', function(event, lFeature, feature) {
 
 jQuery(document).bind('leaflet.map', function(event, map, lMap) {
 
-  function filterHTML(id) {
-    const html =
-    `<filter id="${id}">
-       <feOffset in="SourceAlpha" result="offOut" dx="0" dy="0"/>
-       <feGaussianBlur in="offOut" result="blurOut" stdDeviation="0">
-         <animate id="blur-on-${id}"  attributeName="stdDeviation" to="7" begin="indefinite" dur="0.40s" fill="freeze"/>
-         <animate id="blur-off-${id}" attributeName="stdDeviation" to="0" begin="indefinite" dur="0.25s" fill="freeze"/>
-       </feGaussianBlur>
-       <feBlend in="SourceGraphic" in2="blurOut" mode="normal"/>
-     </filter>`;
-    return html
-  }
-
+  const useTween = map.settings.useTweenMaxForShadowOnHover;
   let activeFeature = null;
+
+  function filterHTML(id) {
+    let html =
+      `<filter id="f${id}">
+         <feOffset in="SourceAlpha" result="offOut" dx="0" dy="0"/>
+         <feGaussianBlur id="blur${id}" in="offOut" result="blurOut" stdDeviation="0">`
+    if (!useTween) {
+      html +=
+        `  <animate id="blur-on${id}"  attributeName="stdDeviation" to="7" begin="indefinite" dur=".45s" fill="freeze"/>
+           <animate id="blur-off${id}" attributeName="stdDeviation" to="0" begin="indefinite" dur=".25s" fill="freeze"/>`
+    }
+    return html +
+      `  </feGaussianBlur>
+         <feBlend in="SourceGraphic" in2="blurOut" mode="normal"/>
+     </filter>`;
+  }
 
   function prepareLayer(layer) {
     // The check for ._path excludes markers (and circles ?)
     if (!layer._path) return
-
-    // Set id on path for use in 'mouseover' handling, below.
-    layer._path.setAttribute('id', 'p' + layer.feature_id)
-
     // Store the current fill opacity, so we can revert to it on mouseout.
     layer._fillOpacity = layer._path.getAttribute('fill-opacity')
     layer._strokeWidth = layer._path.getAttribute('stroke-width')
 
-    if (map.settings.polygonAddShadowOnHover && layer.feature_id) {
-      let svgs = document.getElementsByTagName('svg')
-      if (svgs.length) {
-        // Create a filter <def> for this polygon, if there isn't one.
-        let defs = document.getElementsByTagName('defs');
-        if (!defs.length) {
-          svgs[0].insertAdjacentHTML('afterbegin', '<defs></defs>')
-          defs = document.getElementsByTagName('defs')
-        }
-        defs[0].insertAdjacentHTML('beforeend', filterHTML('f' + layer.feature_id))
+    const id = layer.feature_id;
+    if (!id) return;
 
-        // Link the path to the filter def.
-        layer._path.setAttribute('filter', 'url(#f' + layer.feature_id + ')')
+    // Set id on path for use in 'mouseover' handling, below.
+    layer._path.setAttribute('id', 'p' + id)
+
+    if (!map.settings.polygonAddShadowOnHover) return;
+
+    let svgs = document.getElementsByTagName('svg')
+    if (svgs.length) {
+      // Create a <filter> inside <def> for this polygon, if there isn't one.
+      let defs = document.getElementsByTagName('defs');
+      if (!defs.length) {
+        svgs[0].insertAdjacentHTML('afterbegin', '<defs></defs>')
+        defs = document.getElementsByTagName('defs')
+      }
+      defs[0].insertAdjacentHTML('beforeend', filterHTML(id))
+
+      // Link the path to the filter def.
+      layer._path.setAttribute('filter', `url(#f${id})`)
+
+      if (useTween) {
+        // Attach TweenMax animation, as opposed to using HTML5 <animate> tag.
+        layer.tweenMaxAnimation = TweenMax.to('#blur' + id, 0.45, { attr: { 'stdDeviation': 7 }, paused: true });
       }
     }
   }
@@ -79,21 +90,31 @@ jQuery(document).bind('leaflet.map', function(event, map, lMap) {
     }
 
     if (map.settings.polygonAddShadowOnHover) {
+      // Remove blur from previous feature, if still lingering.
       if (activeFeature) {
-        // Remove blur from previous feature, if still lingering.
-        const blurOff = document.getElementById("blur-off-f" + activeFeature.feature_id);
-        if (blurOff) blurOff.beginElement();
-      }
-      const blur = document.getElementById("blur-on-f" + layer.feature_id)
-      if (blur) {
-        const gs = document.getElementsByTagName("g")
-        if (gs) {
-           const path = document.getElementById("p" + layer.feature_id)
-          // appendChild() is so that the hovered element is on top and
-          // will show shadow on all sides.
-          gs[0].appendChild(path)
+        if (activeFeature.tweenMaxAnimation) {
+          activeFeature.tweenMaxAnimation.reverse();
         }
-        blur.beginElement()
+        else {
+          const blurOff = document.getElementById("blur-off" + activeFeature.feature_id);
+          if (blurOff) blurOff.beginElement();
+        }
+      }
+
+      // appendChild() so that the hovered element is on top and will show
+      // blur/shadow effect on all sides.
+      const gs = document.getElementsByTagName("g")
+      if (gs) {
+        const path = document.getElementById("p" + layer.feature_id)
+        gs[0].appendChild(path)
+      }
+
+      if (layer.tweenMaxAnimation) {
+        layer.tweenMaxAnimation.play();
+      }
+      else {
+        const blur = document.getElementById("blur-on" + layer.feature_id)
+        if (blur) blur.beginElement()
       }
     }
 
@@ -118,8 +139,13 @@ jQuery(document).bind('leaflet.map', function(event, map, lMap) {
     }
 
     if (map.settings.polygonAddShadowOnHover) {
-      const blur = document.getElementById("blur-off-f" + activeFeature.feature_id)
-      if (blur) blur.beginElement()
+      if (activeFeature.tweenMaxAnimation) {
+        activeFeature.tweenMaxAnimation.reverse();
+      }
+      else {
+        const blur = document.getElementById("blur-off" + activeFeature.feature_id)
+        if (blur) blur.beginElement()
+      }
     }
 
     activeFeature = null
@@ -147,7 +173,20 @@ jQuery(document).bind('leaflet.map', function(event, map, lMap) {
       }
     })
   }
+/*
+  jQuery("svg path").each(function(i, path) {
 
+    const id = '#blur' + path.id.substr(1)
+    var tweenMaxAnimation = TweenMax.to(id, 0.4, { attr: { 'stdDeviation': 4 }, paused: true });
+
+    jQuery(path).hover(function() {
+      jQuery(path).appendTo("svg g");
+      tweenMaxAnimation.play()
+    }, function() {
+      tweenMaxAnimation.reverse()
+    })
+  });
+*/
   // Safety net: when pointer moves out from map, terminate active decoration.
   lMap.on('mouseout', function(e) {
     // Take down popups here?
@@ -161,7 +200,7 @@ jQuery(document).bind('leaflet.map', function(event, map, lMap) {
         weight     : activeFeature._strokeWidth,
       })
     }
-    if (map.settings.polygonAddShadowOnHover) {
+    if (map.settings.polygonAddShadowOnHover && !activeFeature.tweenMaxAnimation) {
       const blurOff = document.getElementById("blur-off-f" + activeFeature.feature_id)
       if (blurOff) blurOff.beginElement()
     }
